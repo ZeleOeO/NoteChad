e5b5a1c05bc82a16bc4b9b5b8dac444a46d8b5ee
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-06-10 10:44:56 +0100

    chore: init

---
+++ src/main/java/com/zele/notechad/NoteChadApplication.java
@@ -0,0 +1,13 @@
+package com.zele.notechad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteChadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteChadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zele/notechad/NoteChadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zele.notechad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteChadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
